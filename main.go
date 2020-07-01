8d82452761d3118cda37f09f51f32ddc9d4b924a
NinjaJc01 <james@cookiehome.co.uk>
2020-07-01 04:17:23 +0100

    Initial upload

--- main.go
+++ main.go
@@ -1,18 +1,77 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/sha512"
+	"database/sql"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"mime"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-func main() {
-	go startServer()
+//User represents a user entity
+type User struct {
+	UserID         int
+	Username       string
+	PasswordHash string
+	Salt           string
+	SessionToken   string
 }
+
+//UserReq is the format of a user creation request
+type UserReq struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+//Note represents the structure of a note in the database
+type Note struct {
+	NoteID  int    `json:"noteID"`
+	UserID  int    `json:"userID"`
+	Title   string `json:"noteTitle"`
+	Content string `json:"noteContent"`
+}
+
+//NoteRequest is similar to Note, but includes a session token to verify the user for creation instead of uid
+type NoteRequest struct {
+	SessionToken string `json:"sessionToken"`
+	Content      string `json:"noteContent"`
+	Title        string `json:"noteTitle"`
+}
+
+
+var database *sql.DB
+
+func main() {
+	//Connect to the db
+	databasePointer, err := sql.Open("sqlite3", "database.db")
+	if err != nil {
+		log.Println(err.Error())
+		log.Fatal("Database connection failed")
+	}
+	database = databasePointer
+	startServer()
+}
+
+func generateToken() string {
+	token := make([]byte, 16)
+	rand.Read(token)
+	return (fmt.Sprintf("%x", token)) //hex encoded token
+}
+
+func generateSalt() string {
+	token := make([]byte, 16)
+	rand.Read(token)
+	return (fmt.Sprintf("%x", token)) //hex encoded salt
+}
+
 func startServer() {
 	portPtr := flag.Int("p", 8081, "Port number to run the server on")
 	flag.Parse()
@@ -25,16 +84,537 @@ func startServer() {
 	//Setup a static router for HTML/CSS/JS
 	mr.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("./resources"))))
 	//CRUD API routes
-	aRouter := apiRouter.PathPrefix("/something").Subrouter()
-	/*A route		*/ aRouter.HandleFunc("/{id}", reqHandler).Methods("POST")
-	/*Another Route	*/ aRouter.HandleFunc("/list", reqHandler).Methods("GET")
+	userRouter := apiRouter.PathPrefix("/user").Subrouter()
+	///*A route		*/ userRouter.HandleFunc("/create", createUser).Methods("POST")
+	/*Log out	*/ userRouter.HandleFunc("/logout",doLogout).Methods("POST")
+	/*Log In	*/ userRouter.HandleFunc("/login", doLogin).Methods("POST")
+	noteRouter := apiRouter.PathPrefix("/note").Subrouter()
+	/*Create		*/ noteRouter.HandleFunc("/new", createNote).Methods("POST")
+	/*List by user	*/ noteRouter.HandleFunc("/list", listNotesForUser).Methods("GET")
+	/*Read one		*/ noteRouter.HandleFunc("/{id}", readNote).Methods("GET")
 	log.Println("Listening for requests")
 	http.ListenAndServe(fmt.Sprintf(":%v", port), mr)
+}
+
+func verifyPass(user User, password string) bool {
+	resultHash := hashPassword(password, user.Salt)
+	return resultHash == user.PasswordHash
+}
+
+func hashPassword(password string, salt string) string {
+	hash := sha512.Sum512([]byte(password + salt))
+	return fmt.Sprintf("%x", hash)
 }
 
 func reqHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(404)
 }
+
 func notFoundHandler(w http.ResponseWriter, r *http.Request) { //Handle 404s
 	w.WriteHeader(404)
+}
+
+func userReqToUser(req UserReq) User {
+	var user User
+	user.UserID = dbNextUserID()
+	user.Username = req.Username
+	user.Salt = generateSalt()
+	user.PasswordHash = hashPassword(req.Password, user.Salt)
+	return user
+}
+
+func doLogout(w http.ResponseWriter, r *http.Request) {
+	//determine user by cookie
+	token, err := r.Cookie("SessionToken")
+	http.SetCookie(w,&http.Cookie{Name: "SessionToken", Value: "", Path:"/"})
+	if err != nil {
+		log.Println(err.Error())
+		w.WriteHeader(401) //No cookie, no auth
+		return
+	}
+	user, status := dbVerifyToken(token.Value)
+	if status != "success" { //the user must be authenticated
+		w.WriteHeader(401)
+		return
+	}
+	status = dbSetToken("", user.UserID)
+	if status != "success" {
+		w.WriteHeader(500)
+		return
+	}
+}
+
+func doLogin(w http.ResponseWriter, r *http.Request) {
+	var (
+		username string
+		password string
+	)
+	contentType := r.Header.Get("Content-Type")
+	if contentType == "application/json" {
+		type Login struct {
+			Username string `json:"username"`
+			Password string `json:"password"`
+		}
+		var creds Login
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			//response["status"] = "Error parsing json"
+			w.WriteHeader(500)
+			//json.NewEncoder(w).Encode(response)
+			return
+		}
+		err = json.Unmarshal(body, &creds)
+		if creds.Username != "" && creds.Password != "" {
+			username = creds.Username
+			password = creds.Password
+		} else {
+			//malformed request
+			//response["status"] = "Wrong credential encoding"
+			w.WriteHeader(400)
+			//json.NewEncoder(w).Encode(response)
+			return
+		}
+	} else if contentType == "application/x-www-form-urlencoded" {
+		username = r.FormValue("username")
+		password = r.FormValue("password")
+	} else {
+		//response["status"] = "Wrong credential encoding"
+		log.Println(contentType)
+		w.WriteHeader(415)
+		//json.NewEncoder(w).Encode(response)
+		return
+	}
+	//log.Println(username,password)
+	//w.Header().Set("Content-Type", "application/json")
+	//Verify user and password
+	user, status := dbGetUserByUsername(username)
+	//on success, set cookie and send them to the flag
+	if status != "success" {
+		hashPassword("nottherealpassword","aSalt")
+		hashPassword("nottherealpassword","aSalt")
+		http.SetCookie(w, &http.Cookie{Name: "Status", Value: "Invalid Username Or Password", Path:"/"})
+		http.Redirect(w,r,"/admin",303)
+		
+		return //error!
+	}
+	if !verifyPass(user,password) {
+		http.SetCookie(w, &http.Cookie{Name: "Status", Value: "Invalid Username Or Password", Path:"/"})
+		http.Redirect(w,r,"/admin",303)
+		return //error!
+	}
+	user.SessionToken = generateToken()
+	status = dbSetToken(user.SessionToken, user.UserID)
+	if status != "success" {
+		http.SetCookie(w, &http.Cookie{Name: "Status", Value: "Failed to set session token", Path:"/"})
+		http.Redirect(w,r,"/admin",303)
+		return //error!
+	}
+	//give them a session token in return and TELL THEM it worked
+	http.SetCookie(w, &http.Cookie{Name: "SessionToken", Value: user.SessionToken, Path:"/"})
+	http.Redirect(w,r,"/flag",303)
+}
+//Functonality removed, as not needed for challenge
+// func createUser(w http.ResponseWriter, r *http.Request) { //
+// 	response := make(map[string]string)
+// 	var userReq UserReq
+// 	//Try to read JSON in, make sure it's correct
+// 	body, err := ioutil.ReadAll(r.Body)
+// 	if err != nil {
+// 		w.WriteHeader(500)
+// 		response["status"] = "Error creating user"
+// 		json.NewEncoder(w).Encode(response)
+// 		return
+// 	}
+// 	err = json.Unmarshal(body, &userReq)
+// 	if err != nil {
+// 		w.WriteHeader(400)
+// 		response["status"] = "Error parsing JSON"
+// 		json.NewEncoder(w).Encode(response)
+// 		log.Println(string(body),userReq)
+// 		return
+// 	}
+// 	newUser := userReqToUser(userReq)
+// 	status := dbCreateUser(newUser)
+// 	if status != "success" {
+// 		w.WriteHeader(500)
+// 		response["status"] = "Error creating user"
+// 		json.NewEncoder(w).Encode(response)
+// 		return
+// 	}
+// 	response["status"] = "success"
+// 	json.NewEncoder(w).Encode(response)
+// 	return
+// }
+
+func createNote(w http.ResponseWriter, r *http.Request) {
+	/*
+		{
+			sessionToken:
+			noteTitle:
+			noteContent:
+		}
+	*/
+	response := make(map[string]string)
+	token, err := r.Cookie("SessionToken")
+	if err != nil {
+		log.Println(err.Error())
+		w.WriteHeader(401) //No cookie, no auth
+		response["status"] = "Unauthenticated"
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+	user, status := dbVerifyToken(token.Value)
+	if status != "success" { //the user must be authenticated
+		w.WriteHeader(401)
+		response["status"] = "Unauthenticated"
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(500)
+		response["status"] = "Error creating note"
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+	var note Note
+	err = json.Unmarshal(body, &note)
+	note.UserID = user.UserID
+	note.NoteID = dbNextNoteID()
+	dbCreateNote(note)
+
+	response["status"] = "Successfully created note"
+	json.NewEncoder(w).Encode(response)
+	return
+}
+func listNotesForUser(w http.ResponseWriter, r *http.Request) {
+	//determine what user by token
+	token, err := r.Cookie("SessionToken")
+	if err != nil {
+		log.Println(err.Error())
+		w.WriteHeader(401) //No cookie, no auth
+		return
+	}
+	user, status := dbVerifyToken(token.Value)
+	if status != "success" {
+		w.WriteHeader(401)
+	}
+	notes, status := dbGetNotesByUserID(user.UserID)
+	if status != "success" {
+		w.WriteHeader(500)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(notes)
+}
+func readNote(w http.ResponseWriter, r *http.Request) { //Note by ID
+	//check perms for the user, notes are private
+	//then check noteid
+	//then write it as JSON to w.
+}
+func updateNote(w http.ResponseWriter, r *http.Request) {
+	return
+}
+func deleteNote(w http.ResponseWriter, r *http.Request) {
+	return
+}
+
+/* DATABASE */
+/* User Database Functions */
+func dbNextUserID() int {
+	res, err := database.Query("SELECT Max(UserID) FROM User")
+	if err != nil {
+		if res !=  nil {
+			res.Close()
+		}
+		log.Println(err.Error())
+		return -1
+	}
+	var UserID int
+	if res.Next() {
+		err = res.Scan(&UserID)
+		if err != nil {
+			res.Close()
+			log.Println(err.Error())
+			return -1
+		}
+	} else {
+		log.Println("No Next ID for User found?")
+		return -1
+	}
+	res.Close()
+	return UserID + 1
+}
+func dbCreateUser(user User) (status string) {
+	statement, err := database.Prepare("INSERT INTO User(UserID, Username, HashedPassword, Salt, SessionToken) values(?,?,?,?,?,?)")
+	if err != nil {
+		log.Println(err.Error())
+		return "DB statement preperation failed"
+	}
+	_, err = statement.Exec(
+		user.UserID,
+		user.Username,
+		user.PasswordHash,
+		user.Salt,
+		user.SessionToken)
+	if err != nil {
+		log.Println(err.Error())
+		return "DB statement execution failed"
+	}
+	return "success"
+}
+
+func dbSetToken(token string, userID int) string {
+	statement, err := database.Prepare("UPDATE User SET SessionToken = ? WHERE UserID = ?")
+	if err != nil {
+		log.Println(err.Error())
+		return "DB statement preperation failed"
+	}
+	_, err = statement.Exec(token, userID)
+	if err != nil {
+		log.Println(err.Error())
+		return "DB execution failed"
+	}
+
+	return "success"
+}
+
+func dbVerifyToken(token string) (User, string) {
+	//Lookup by Token
+	if token == "" {
+		return User{}, "nil token"
+	}
+	statement, err := database.Prepare("SELECT * FROM User WHERE SessionToken = ?")
+	if err != nil {
+		log.Println(err.Error())
+		return User{}, "DB statement preperation failed"
+	}
+	res, err := statement.Query(token)
+	if err != nil {
+		res.Close()
+		log.Println(err.Error())
+		return User{}, "DB execution failed"
+	}
+	var user User
+	for res.Next() { //Technically Identical UIDs will give the last
+		var (
+			UserID         int
+			Username       string
+			Realname       string
+			PasswordHash string
+			Salt           string
+			SessionToken   string
+		)
+
+		err = res.Scan(&UserID, &Username, &Realname, &PasswordHash, &Salt, &SessionToken)
+		if err != nil {
+			res.Close()
+			log.Println(err.Error())
+			return User{}, "DB query reading failed"
+		}
+		user = User{
+			UserID:         UserID,
+			Username:       Username,
+			PasswordHash: PasswordHash,
+			Salt:           Salt,
+			SessionToken:   SessionToken,
+		}
+	}
+	if user.UserID == 0 {
+		return User{}, "Invalid token"
+	}
+	res.Close()
+	return user, "success"
+}
+
+func dbGetUserByUsername(username string) (User, string) {
+	statement, err := database.Prepare("SELECT * FROM User WHERE Username = ?")
+	if err != nil {
+		log.Println(err.Error())
+		return User{}, "DB statement preperation failed"
+	}
+	res, err := statement.Query(username)
+	if err != nil {
+		res.Close()
+		log.Println(err.Error())
+		return User{}, "DB execution failed"
+	}
+	var user User
+	for res.Next() { //Technically Identical Usernames will give the last
+		var (
+			UserID         int
+			Username       string
+			Realname       string
+			PasswordHash string
+			Salt           string
+			SessionToken   string
+		)
+
+		err = res.Scan(&UserID, &Username, &Realname, &PasswordHash, &Salt, &SessionToken)
+		if err != nil {
+			res.Close()
+			log.Println(err.Error())
+			return User{}, "DB query reading failed"
+		}
+		user = User{
+			UserID:         UserID,
+			Username:       Username,
+			PasswordHash: PasswordHash,
+			Salt:           Salt,
+			SessionToken:   SessionToken,
+		}
+	}
+	if user.UserID == 0 {
+		return User{}, "User not found"
+	}
+	res.Close()
+	return user, "success"
+}
+
+/*Note Database Functions*/
+func dbNextNoteID() int {
+	res, err := database.Query("SELECT Max(NoteID) FROM Note")
+	if err != nil {
+		log.Println(err.Error())
+		return -1
+	}
+	var NoteID int
+	if res.Next() {
+		err = res.Scan(&NoteID)
+		if err != nil {
+			log.Println(err.Error())
+			return -1
+		}
+	} else {
+		res.Close()
+		log.Println("No Next ID for Note found?")
+		return -1
+	}
+	res.Close()
+	return NoteID + 1
+}
+
+func dbCreateNote(note Note) string {
+	statement, err := database.Prepare(
+		"INSERT INTO Note(NoteID, UserID, NoteTitle, NoteContent) values(?,?,?,?)")
+	if err != nil {
+		log.Println(err.Error())
+		return "DB statement preperation failed"
+	}
+	_, err = statement.Exec(
+		note.NoteID,
+		note.UserID,
+		note.Title,
+		note.Content)
+	if err != nil {
+		log.Println(err.Error())
+		return "DB statement execution failed"
+	}
+	return "success"
+}
+
+func dbGetNotes() ([]Note, string) {
+	var noteList []Note
+	res, err := database.Query("SELECT * FROM Note")
+	if err != nil {
+		log.Println(err.Error())
+		return nil, "DB statement execution failed"
+	}
+	for res.Next() {
+		var (
+			NoteID  int
+			UserID  int
+			Title   string
+			Content string
+		)
+		var note Note
+		err = res.Scan(&NoteID, &UserID, &Title, &Content)
+		if err != nil {
+			res.Close()
+			log.Println(err.Error())
+			return nil, "DB statement execution failed"
+		}
+		note = Note{
+			NoteID:  NoteID,
+			UserID:  UserID,
+			Title:   Title,
+			Content: Content,
+		}
+		noteList = append(noteList, note)
+	}
+	res.Close()
+	return noteList, "success"
+}
+
+func dbGetNoteByNoteID(id int) (Note, string) {
+	statement, err := database.Prepare("SELECT * FROM Note WHERE NoteID = ?")
+	if err != nil {
+		log.Println(err.Error())
+		return Note{}, "DB statement preperation failed"
+	}
+	res, err := statement.Query(id)
+	if err != nil {
+		log.Println(err.Error())
+		return Note{}, "DB execution failed"
+	}
+	var note Note
+	for res.Next() {
+		var (
+			NoteID  int
+			UserID  int
+			Title   string
+			Content string
+		)
+		err = res.Scan(&NoteID, &UserID, &Title, &Content)
+		if err != nil {
+			log.Println(err.Error())
+			res.Close()
+			return Note{}, "DB statement execution failed"
+		}
+		note = Note{
+			NoteID:  NoteID,
+			UserID:  UserID,
+			Title:   Title,
+			Content: Content,
+		}
+	}
+	res.Close()
+	return note, "success"
+}
+
+func dbGetNotesByUserID(id int) ([]Note, string) {
+	statement, err := database.Prepare("SELECT * FROM Note WHERE UserID = ?")
+	if err != nil {
+		log.Println(err.Error())
+		return nil, "DB statement preperation failed"
+	}
+	res, err := statement.Query(id)
+	if err != nil {
+		log.Println(err.Error())
+		return nil, "DB execution failed"
+	}
+	var notes []Note
+	for res.Next() {
+		var (
+			note    Note
+			NoteID  int
+			UserID  int
+			Title   string
+			Content string
+		)
+		err = res.Scan(&NoteID, &UserID, &Title, &Content)
+		if err != nil {
+			log.Println(err.Error())
+			res.Close()
+			return nil, "DB statement execution failed"
+		}
+		note = Note{
+			NoteID:  NoteID,
+			UserID:  UserID,
+			Title:   Title,
+			Content: Content,
+		}
+		notes = append(notes, note)
+	}
+	res.Close()
+	return notes, "success"
 }
